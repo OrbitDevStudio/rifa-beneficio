05852e39a9623f75e53d0c61b8f7f02e2762a10f
yefersonalejandroheredia38-crypto <yefersonalejandroheredia38@gmail.com>
2026-03-08 09:34:51 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const totalNumeros = 1000; // Puedes cambiar la cantidad
+const contenedor = document.getElementById('contenedor-rifa');
+
+// Generar números
+for (let i = 1; i <= totalNumeros; i++) {
+    let div = document.createElement('div');
+    div.classList.add('numero');
+    div.innerText = i;
+    
+    div.onclick = () => {
+        if(!div.classList.contains('vendido')) {
+            confirmarCompra(i, div);
+        }
+    };
+    contenedor.appendChild(div);
+}
+
+function confirmarCompra(num, elemento) {
+    if(confirm(`¿Deseas apartar el número ${num}?`)) {
+        elemento.classList.add('vendido');
+        // Aquí conectarías con tu base de datos o WhatsApp
+        window.open(`https://wa.me/+584122415696text=Hola, quiero el número ${num} de la rifa`);
+    }
+}
